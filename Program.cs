306eee365d415f1b26a3ea3f02e3dd38eefac917
AnnaS <anna-c-91@mail.ru>
2022-12-20 20:46:31 +1200

    Решена вторая задача домашнего задания

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int max=1;
+Console.WriteLine ("Введите первое число");
+string num1=Console.ReadLine();
+int number1=int.Parse(num1);
+Console.WriteLine ("Введите второе число");
+string num2=Console.ReadLine();
+int number2=int.Parse(num2);
+Console.WriteLine ("Введите третье число");
+string num3=Console.ReadLine();
+int number3=int.Parse(num3);
+if (number1>number2) max=number1;
+else max=number2;
+if (max<number3) max=number3;
+Console.Write ("Максимальное число "+max);
+
+
